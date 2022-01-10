b040e5b44db8357e2edb6bed9ad15ee94d8cfc4e
James <james@prestwi.ch>
2022-01-09 20:52:30 -0800

    fixes: wallet and process flow

--- src/store/modules/sdk.ts
+++ src/store/modules/sdk.ts
@@ -1,279 +1,286 @@
-import { MutationTree, ActionTree, GetterTree } from 'vuex'
-import { ethers, BigNumber, BytesLike } from 'ethers'
-import { mainnet, dev, NomadContext } from '@nomad-xyz/sdk'
-import { TransferMessage } from '@nomad-xyz/sdk/nomad/messages/BridgeMessage'
-import { TokenIdentifier } from '@nomad-xyz/sdk/nomad'
+import { MutationTree, ActionTree, GetterTree } from "vuex";
+import { ethers, BigNumber, BytesLike } from "ethers";
+import { mainnet, dev, NomadContext } from "@nomad-xyz/sdk";
+import { TransferMessage } from "@nomad-xyz/sdk/nomad/messages/BridgeMessage";
+import { TokenIdentifier } from "@nomad-xyz/sdk/nomad";
 
-import { TXData } from './transactions'
-import { RootState } from '@/store'
-import * as types from '@/store/mutation-types'
-import { networks } from '@/config/index'
-import { getBalance, getNativeBalance, getERC20Balance } from '@/utils/balance'
-import { isNativeToken, getNetworkByDomainID } from '@/utils/index'
-import { NetworkMetadata } from '@/config/config.types'
+import { TXData } from "./transactions";
+import { RootState } from "@/store";
+import * as types from "@/store/mutation-types";
+import { networks } from "@/config/index";
+import { getBalance, getNativeBalance, getERC20Balance } from "@/utils/balance";
+import { isNativeToken, getNetworkByDomainID } from "@/utils/index";
+import { NetworkMetadata } from "@/config/config.types";
 
-const isProduction = process.env.VUE_APP_NOMAD_ENVIRONMENT === 'production'
-const s3URL = 'https://nomadxyz-development-proofs.s3.us-west-2.amazonaws.com/'
+const isProduction = process.env.VUE_APP_NOMAD_ENVIRONMENT === "production";
+const s3URL = "https://nomadxyz-development-proofs.s3.us-west-2.amazonaws.com/";
 
 function _instantiateNomad(isProduction: boolean): NomadContext {
   // configure for mainnet/testnet
-  const nomadContext: NomadContext = isProduction ? mainnet : dev
+  const nomadContext: NomadContext = isProduction ? mainnet : dev;
 
   // register rpc provider and signer for each network
   Object.values(networks).forEach(({ name, rpcUrl }) => {
-    nomadContext.registerRpcProvider(name, rpcUrl)
-  })
+    nomadContext.registerRpcProvider(name, rpcUrl);
+  });
 
-  return nomadContext
+  return nomadContext;
 }
 
-let nomad: NomadContext
+let nomad: NomadContext;
 
 export interface SendData {
-  isNative: boolean
-  originNetwork: number
-  destNetwork: number
-  asset: TokenIdentifier
-  amnt: number
-  recipient: string
-  gasLimit?: number
+  isNative: boolean;
+  originNetwork: number;
+  destNetwork: number;
+  asset: TokenIdentifier;
+  amnt: number;
+  recipient: string;
+  gasLimit?: number;
 }
 
 export interface SDKState {
-  balance: BigNumber | null
-  sending: boolean
+  balance: BigNumber | null;
+  sending: boolean;
 }
 
 const state: SDKState = {
   balance: null,
   sending: false,
-}
+};
 
 const mutations = <MutationTree<SDKState>>{
   [types.SET_BALANCE](state: SDKState, balance: BigNumber | null) {
-    console.log('{dispatch} set balance: ', balance)
-    state.balance = balance
+    console.log("{dispatch} set balance: ", balance);
+    state.balance = balance;
   },
 
   [types.SET_SENDING](state: SDKState, sending: boolean) {
-    console.log('{dispatch} transaction send in process: ', sending)
-    state.sending = sending
+    console.log("{dispatch} transaction send in process: ", sending);
+    state.sending = sending;
   },
-}
+};
 
 const actions = <ActionTree<SDKState, RootState>>{
   instantiateNomad() {
-    console.log('called on mount, production = ', isProduction)
+    console.log("called on mount, production = ", isProduction);
     try {
-      nomad = _instantiateNomad(isProduction)
-      console.log('nomad after instantiating', nomad)
+      nomad = _instantiateNomad(isProduction);
+      console.log("nomad after instantiating", nomad);
     } catch (e) {
-      throw new Error('Couldn\'t setup Nomad')
+      throw new Error("Couldn't setup Nomad");
     }
   },
 
   async getBalanceFromWallet({ rootState, commit }) {
-    console.log('gettingbalanceFromwallet')
+    console.log("gettingbalanceFromwallet");
     // get current network domain
-    const networkName = rootState.userInput.originNetwork
-    const network = networks[networkName]
-    const domain = network.domainID
-    const address = rootState.wallet.address
-    const token = rootState.userInput.token
+    const networkName = rootState.userInput.originNetwork;
+    const network = networks[networkName];
+    const domain = network.domainID;
+    const address = rootState.wallet.address;
+    const token = rootState.userInput.token;
 
-    let balance
+    let balance;
     if (token.tokenIdentifier.domain === networkName) {
       if (isNativeToken(networkName, token)) {
-        console.log('getting native token balance')
+        console.log("getting native token balance");
         try {
-          balance = await getNativeBalance(nomad, network.name, address)
+          balance = await getNativeBalance(nomad, network.name, address);
         } catch (e) {
-          balance = 0
-          console.error(e)
-          console.log(`no balance for ${token.name}`)
+          balance = 0;
+          console.error(e);
+          console.log(`no balance for ${token.name}`);
         }
       } else {
-        console.log('getting balance of ERC20 token: ', token.name)
-        const provider = nomad.getProvider(network.name)
+        console.log("getting balance of ERC20 token: ", token.name);
+        const provider = nomad.getProvider(network.name);
         try {
           balance = await getERC20Balance(
             provider as ethers.providers.Web3Provider,
             token.tokenIdentifier.id as string,
             address
-          )
+          );
         } catch (e) {
-          balance = 0
-          console.error(e)
-          console.log(`no balance for ${token.name}`)
+          balance = 0;
+          console.error(e);
+          console.log(`no balance for ${token.name}`);
         }
       }
     } else {
-      console.log('getting representational token balance')
+      console.log("getting representational token balance");
       try {
         balance = await getBalance(
           nomad,
           token.tokenIdentifier,
           address,
           domain
-        )
+        );
       } catch (e) {
         // there is no balance so it errors
         // should return 0
-        balance = 0
-        console.error(e)
-        console.log(`no balance for ${token.name}`)
+        balance = 0;
+        console.error(e);
+        console.log(`no balance for ${token.name}`);
       }
     }
 
-    commit(types.SET_BALANCE, balance)
+    commit(types.SET_BALANCE, balance);
   },
 
   registerSigner({ commit }, network: NetworkMetadata) {
-    console.log('registering signer for ', network)
-    const networkName = network.name
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    const newSigner = provider.getSigner()
+    console.log("registering signer for ", network);
+    const networkName = network.name;
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const newSigner = provider.getSigner();
 
-    nomad.clearSigners()
-    const missingProviders = nomad.missingProviders
+    nomad.clearSigners();
+    const missingProviders = nomad.missingProviders;
     missingProviders.forEach((domain: number) => {
-      const network = getNetworkByDomainID(domain)
-      nomad.registerRpcProvider(networkName, network.rpcUrl)
-    })
+      const network = getNetworkByDomainID(domain);
+      nomad.registerRpcProvider(networkName, network.rpcUrl);
+    });
 
-    nomad.registerSigner(networkName, newSigner)
+    nomad.registerSigner(networkName, newSigner);
   },
 
-  async send(
-    { commit },
-    payload: SendData
-  ): Promise<TransferMessage | null> {
-    console.log('sending...', payload)
-    commit(types.SET_SENDING, true)
-    const {
-      isNative,
-      originNetwork,
-      destNetwork,
-      asset,
-      amnt,
-      recipient,
-    } = payload
+  async send({ commit }, payload: SendData): Promise<TransferMessage | null> {
+    console.log("sending...", payload);
+    commit(types.SET_SENDING, true);
+    const { isNative, originNetwork, destNetwork, asset, amnt, recipient } =
+      payload;
 
-    const originDomain = nomad.resolveDomain(originNetwork)
-    const destDomain = nomad.resolveDomain(destNetwork)
+    const originDomain = nomad.resolveDomain(originNetwork);
+    const destDomain = nomad.resolveDomain(destNetwork);
 
-    let transferMessage
+    let transferMessage;
     try {
       // if ETH Helper contract exists, native token must be wrapped
       // before sending, use sendNative
-      const ethHelper = nomad.getBridge(originDomain)?.ethHelper
+      const ethHelper = nomad.getBridge(originDomain)?.ethHelper;
       if (ethHelper && isNative) {
-        console.log('send native')
+        console.log("send native");
         transferMessage = await nomad.sendNative(
           originDomain,
           destDomain,
           amnt,
           recipient
-        )
+        );
       } else {
-        console.log('send ERC-20')
+        console.log("send ERC-20");
         transferMessage = await nomad.send(
           originDomain,
           destDomain,
           asset,
           amnt,
-          recipient,
-        )
+          recipient
+        );
       }
-      console.log('tx sent!!!!!!!!!!!!', transferMessage)
-      commit(types.SET_SENDING, false)
-      return transferMessage
+      console.log("tx sent!!!!!!!!!!!!", transferMessage);
+      commit(types.SET_SENDING, false);
+      return transferMessage;
     } catch (e) {
-      console.error(e)
+      console.error(e);
     }
 
-    commit(types.SET_SENDING, false)
-    return null
+    commit(types.SET_SENDING, false);
+    return null;
   },
-  async processTx ({ dispatch }, tx: any) {
-    // register signer
-    await dispatch('registerSigner', networks['kovan'])
-
+  async processTx({ dispatch }, tx: any) {
     // get transfer message
-    const { origin, hash } = tx
-    const message = await TransferMessage.singleFromTransactionHash(nomad, origin, hash)
+    const { origin, hash } = tx;
+    const message = await TransferMessage.singleFromTransactionHash(
+      nomad,
+      origin,
+      hash
+    );
+
+    // register signer
+    const domainName = message.context.resolveDomainName(message.destination)!;
+    await dispatch("switchNetwork", domainName);
 
     // get proof
-    const res = await fetch(`${s3URL}${origin}_${message.leafIndex.toString()}`)
-    const data = (await res.json()) as any
-    console.log('proof: ', data)
-
+    const res = await fetch(
+      `${s3URL}${origin}_${message.leafIndex.toString()}`
+    );
+    const data = (await res.json()) as any;
+    console.log("proof: ", data);
     // get replica contract
-    const core = nomad.mustGetCore(message.origin)
-    const replica = core?.getReplica(message.destination)
+    const replica = nomad
+      .mustGetCore(message.destination)
+      .getReplica(message.origin);
+
+    console.log(replica!.signer);
 
     // prove and process
     try {
-      await replica!.proveAndProcess(data.message as BytesLike, data.proof.path, data.proof.index)
-      console.log('PROCESSED!!!!')
-    } catch(e) {
-      console.log(e)
+      await replica!.proveAndProcess(
+        data.message as BytesLike,
+        data.proof.path,
+        data.proof.index
+      );
+      console.log("PROCESSED!!!!");
+    } catch (e) {
+      console.log(e);
     }
   },
-}
+};
 
 const getters = <GetterTree<SDKState, RootState>>{
   getGasPrice: () => async (network: string | number) => {
     try {
-      const provider = nomad.getProvider(network)
-      const gasPrice = await provider?.getGasPrice()
-      return gasPrice
+      const provider = nomad.getProvider(network);
+      const gasPrice = await provider?.getGasPrice();
+      return gasPrice;
     } catch (e) {
-      console.error(e)
+      console.error(e);
     }
   },
 
-  getTxMessage: () => async (tx: TXData): Promise<TransferMessage> => {
-    const { network, hash } = tx
-    let message
+  getTxMessage:
+    () =>
+    async (tx: TXData): Promise<TransferMessage> => {
+      const { network, hash } = tx;
+      let message;
 
-    try {
-      message = await TransferMessage.singleFromTransactionHash(
-        nomad,
-        network,
-        hash
-      )
-    } catch (e) {
-      console.error(e)
-    }
+      try {
+        message = await TransferMessage.singleFromTransactionHash(
+          nomad,
+          network,
+          hash
+        );
+      } catch (e) {
+        console.error(e);
+      }
 
-    return message as TransferMessage
-  },
+      return message as TransferMessage;
+    },
 
   resolveDomain: (state: SDKState) => (network: string) => {
-    return nomad.resolveDomain(network)
+    return nomad.resolveDomain(network);
   },
 
   resolveDomainName: (state: SDKState) => (network: number) => {
-    return nomad.resolveDomainName(network)
+    return nomad.resolveDomainName(network);
   },
 
-  resolveRepresentation: (state: SDKState) => async (network: string, token: TokenIdentifier) => {
-    let bridgeToken
+  resolveRepresentation:
+    (state: SDKState) => async (network: string, token: TokenIdentifier) => {
+      let bridgeToken;
 
-    try {
-      bridgeToken = await nomad.resolveRepresentation(network, token)
-    } catch (e) {
-      console.error(e)
-    }
+      try {
+        bridgeToken = await nomad.resolveRepresentation(network, token);
+      } catch (e) {
+        console.error(e);
+      }
 
-    return bridgeToken
-  },
-}
+      return bridgeToken;
+    },
+};
 
 export default {
   state,
   mutations,
   actions,
   getters,
-}
+};
--- src/store/modules/wallet.ts
+++ src/store/modules/wallet.ts
@@ -2,160 +2,163 @@
  * Wallet Module contains most information that comes from a user's wallet
  * This is also a good module to look at for how to write a Vuex module
  */
-import { MutationTree, ActionTree } from 'vuex'
-import { RootState } from '@/store'
-import * as types from '@/store/mutation-types'
-import { networks } from '@/config/index'
-import * as mmUtils from '@/utils/metamask'
-import { getNetworkByChainID, getOnlyOtherNetwork, nullToken } from '@/utils'
+import { MutationTree, ActionTree } from "vuex";
+import { RootState } from "@/store";
+import * as types from "@/store/mutation-types";
+import { networks } from "@/config/index";
+import * as mmUtils from "@/utils/metamask";
+import { getNetworkByChainID, getOnlyOtherNetwork, nullToken } from "@/utils";
 
 export interface WalletState {
-  connected: boolean
-  address: string
+  connected: boolean;
+  address: string;
 }
 
 const state = (): WalletState => ({
   connected: false,
-  address: localStorage.getItem('wallet_address') || '',
-})
+  address: localStorage.getItem("wallet_address") || "",
+});
 
 const mutations = <MutationTree<WalletState>>{
   [types.SET_WALLET_CONNECTION](state: WalletState, connected: boolean) {
-    console.log('{dispatch} set wallet connection: ', connected)
-    state.connected = connected
+    console.log("{dispatch} set wallet connection: ", connected);
+    state.connected = connected;
   },
 
   [types.SET_WALLET_ADDRESS](state: WalletState, address: string) {
-    console.log('{dispatch} set wallet address: ', address)
-    state.address = address
-    localStorage.setItem('wallet_address', address)
+    console.log("{dispatch} set wallet address: ", address);
+    state.address = address;
+    localStorage.setItem("wallet_address", address);
   },
-}
+};
 
 const actions = <ActionTree<WalletState, RootState>>{
   async connectWallet({ dispatch, commit, state }) {
     // check if already connected
     if (state.connected) {
-      console.log('already connected to wallet')
-      return
+      console.log("already connected to wallet");
+      return;
     }
 
     // if window.ethereum does not exist, do not connect
-    const { ethereum } = window
-    if (!ethereum) return
+    const { ethereum } = window;
+    if (!ethereum) return;
 
     try {
       // connect Metamask
-      await window.ethereum.request({ method: 'eth_requestAccounts' })
+      await window.ethereum.request({ method: "eth_requestAccounts" });
     } catch (e) {
-      console.error(e)
-      throw new Error('Couldn\'t connect to metamask')
+      console.error(e);
+      throw new Error("Couldn't connect to metamask");
     }
 
     // get provider/signer
-    const provider = await mmUtils.getMetamaskProvider()
-    const signer = await provider.getSigner()
+    const provider = await mmUtils.getMetamaskProvider();
+    const signer = await provider.getSigner();
 
     // get and set address
-    const address = await signer.getAddress()
-    dispatch('setWalletAddress', address)
-    dispatch('setDestinationAddress', address, { root: true }) // initialize destination address
+    const address = await signer.getAddress();
+    dispatch("setWalletAddress", address);
+    dispatch("setDestinationAddress", address, { root: true }); // initialize destination address
 
     // set network, if supported
-    const { chainId } = await provider.ready
+    const { chainId } = await provider.ready;
     try {
-      const network = getNetworkByChainID(chainId)
-      dispatch('setWalletNetwork', network.name)
+      const network = getNetworkByChainID(chainId);
+      dispatch("setWalletNetwork", network.name);
     } catch (e) {
-      console.log('network not supported')
+      console.log("network not supported");
     }
 
     // wallet connected
-    commit(types.SET_WALLET_CONNECTION, true)
+    commit(types.SET_WALLET_CONNECTION, true);
   },
 
   setWalletAddress({ commit }, address: string) {
-    commit(types.SET_WALLET_ADDRESS, address)
+    commit(types.SET_WALLET_ADDRESS, address);
   },
 
   // when user changes network in Metamask
   setWalletNetwork({ commit, dispatch, rootState }, networkName: string) {
-    dispatch('setOriginNetwork', networkName)
+    dispatch("setOriginNetwork", networkName);
 
     // if there are exactly 2 networks, auto select the destination network
     if (Object.keys(networks).length === 2) {
-      dispatch('setDestinationNetwork', getOnlyOtherNetwork(networkName))
+      dispatch("setDestinationNetwork", getOnlyOtherNetwork(networkName));
     }
 
-    const { token } = rootState.userInput
+    const { token } = rootState.userInput;
 
     // if token has not been selected, no further action required
-    if (!token.symbol) return
+    if (!token.symbol) return;
 
     // if token is not available on network, clear token and balance
     if (token.nativeOnly && token.nativeNetwork !== networkName) {
-      commit(types.SET_TOKEN, nullToken)
-      commit(types.SET_BALANCE, null)
-      return
+      commit(types.SET_TOKEN, nullToken);
+      commit(types.SET_BALANCE, null);
+      return;
     }
 
     // if token is selected and available on new network, get balance
-    dispatch('getBalanceFromWallet')
+    dispatch("getBalanceFromWallet");
   },
 
   async switchNetwork({ dispatch, state, commit }, networkName: string) {
-    console.log('set wallet network')
+    console.log(`setting wallet network to ${networkName}`);
     if (!state.connected) {
-      dispatch('connectWallet')
+      dispatch("connectWallet");
     }
 
     // if window.ethereum does not exist, do not instantiate nomad
-    const { ethereum } = window
-    if (!ethereum) return
+    const { ethereum } = window;
+    if (!ethereum) return;
 
-    const network = networks[networkName]
-    const hexChainId = '0x' + network.chainID.toString(16)
+    const network = networks[networkName];
+    const hexChainId = "0x" + network.chainID.toString(16);
     try {
+      console.log(`dispatching wallet_switchEthereumChain(${hexChainId})`);
       await ethereum.request({
-        method: 'wallet_switchEthereumChain',
+        method: "wallet_switchEthereumChain",
         params: [{ chainId: hexChainId }],
-      })
+      });
     } catch (switchError: any) {
+      console.log(switchError.code);
       // This error code indicates that the chain has not been added to MetaMask.
       if (switchError.code === 4902) {
         try {
+          const param = {
+            chainId: hexChainId,
+            rpcUrls: [network.rpcUrl],
+            chainName: network.name,
+            nativeCurrency: {
+              name: network.nativeToken.name,
+              symbol: network.nativeToken.symbol,
+              decimals: network.nativeToken.decimals,
+            },
+          };
+          console.log(`dispatching wallet_addEthereumChain(${param})`);
           await ethereum.request({
-            method: 'wallet_addEthereumChain',
-            params: [
-              {
-                chainId: hexChainId,
-                rpcUrls: [network.rpcUrl],
-                chainName: network.name,
-                nativeCurrency: {
-                  name: network.nativeToken.name,
-                  symbol: network.nativeToken.symbol,
-                  decimals: network.nativeToken.decimals,
-                },
-              },
-            ],
-          })
+            method: "wallet_addEthereumChain",
+            params: [param],
+          });
         } catch (addError: any) {
           // TODO: handle "add" error, alert?
-          console.error(addError)
-          throw addError
+          console.error(addError);
+          throw addError;
         }
       }
-      console.error(switchError)
-      throw switchError
+      console.error(switchError);
+      throw switchError;
       // TODO: handle other "switch" errors, alert?
     }
 
-    dispatch('setWalletNetwork', network.name)
+    await dispatch("registerSigner", network);
+    await dispatch("setWalletNetwork", network.name);
   },
-}
+};
 
 export default {
   state,
   mutations,
   actions,
-}
+};
