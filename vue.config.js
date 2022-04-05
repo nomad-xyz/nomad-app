432f310a041743c8301a73fdcb377a9a68c98c9b
ErinHales <mcbridee093@gmail.com>
2022-04-04 18:34:52 -0600

    fix: some imports

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
+const webpack = require('webpack');
 
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -11,12 +12,12 @@ module.exports = defineConfig({
         },
       ],
     },
-    // plugins: [
-    //   new webpack.ProvidePlugin({
-    //     process: 'process/browser',
-    //   }),
-    // ],
-    // devtool: 'source-map',
+    plugins: [
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+      }),
+    ],
+    devtool: 'source-map',
     experiments: {
       syncWebAssembly: true,
     },
