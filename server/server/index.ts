801bf1d6eeb9619b9ae2013f295a67931568166f
ErinHales <mcbridee093@gmail.com>
2022-05-12 10:48:39 -0600

    fix: server post, add any script

--- server/server/index.ts
+++ server/server/index.ts
@@ -2,7 +2,7 @@ const express = require('express')
 var bodyParser = require('body-parser')
 const { db } = require('./api')
 const app = express()
-const { PORT } = process.env
+const { SERVER_PORT } = process.env
 
 type AgreeTerms = {
   body: {
@@ -26,6 +26,6 @@ app.get('/agreement/:address', async (req: any, res: any) => {
   return agreement ? res.json(agreement.timestamp) : res.status(404)
 })
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`)
+app.listen(SERVER_PORT, () => {
+  console.log(`App listening on port ${SERVER_PORT}`)
 })
