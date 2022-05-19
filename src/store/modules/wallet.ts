ee6ba69401c327d8a1f324797183648dc6013206
Imti Majeed <imtiazmaj@gmail.com>
2022-05-19 15:50:38 -0400

    fix: set wallet network after adding a new chain to wallet

--- src/store/modules/wallet.ts
+++ src/store/modules/wallet.ts
@@ -199,6 +199,7 @@ const actions = <ActionTree<WalletState, RootState>>{
             },
           ],
         })
+        dispatch('setWalletNetwork', network.name)
       } else {
         throw switchError
       }
