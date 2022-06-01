f984368eb2fc2c24d44c4e215abfc87eaab0ca29
Erin Hales <36751902+ErinHales@users.noreply.github.com>
2022-06-01 11:31:36 -0600

    feature: tx history (#339)
    * update: gitignore
    
    * feature: start nomad history
    
    * add: basic tx history
    
    * refactor: history, manualProcess transactions
    
    * update: pagination
    
    * add: amount
    
    * fix: amount and scroll behavior
    
    * fix: page has no more transactions
    
    * fix: tx history recipient query
    
    * fix: pagination
    
    * lint
    
    * fix: tweak pagination logic

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,7 @@ import { BigNumber, utils } from 'ethers'
 
 import { networks, tokens } from '@/config'
 import { NetworkMetadata, TokenMetadata, NetworkName } from '@/config/types'
+import { TokenIdentifier } from '@nomad-xyz/sdk-bridge'
 
 const coinGeckoIds = Object.values(tokens).map((t) => t.coinGeckoId)
 
@@ -55,9 +56,24 @@ export function truncateAddr(addr: string): string {
 }
 
 export function fromBytes32(addr: string): string {
-  // trim 12 bytes from beginning plus '0x'
-  const short = addr.slice(26)
-  return `0x${short}`
+  if (addr.length === 42) return addr
+  if (addr.length === 66) {
+    // trim 12 bytes from beginning plus '0x'
+    const short = addr.slice(26)
+    return `0x${short}`
+  }
+  throw new Error('invalid address length, cannot convert to 20 bytes')
+}
+
+export function toBytes32(addr: string): string {
+  if (addr.length === 66) return addr
+  if (addr.length === 42) {
+    // trim 12 bytes from beginning plus '0x'
+    const short = addr.slice(2)
+    console.log('short', short)
+    return `0x000000000000000000000000${short}`
+  }
+  throw new Error('invalid address length, cannot convert to 20 bytes')
 }
 
 export function toBytes32(addr: string): string {
@@ -218,6 +234,22 @@ export function getTokenBySymbol(symbol: string): TokenMetadata {
   for (const t in tokens) {
     const token = tokens[t]
     if (token.symbol === symbol) {
+      return token
+    }
+  }
+  throw new Error('token asset not found')
+}
+
+export function getTokenByTokenID(
+  tokenID: TokenIdentifier
+): TokenMetadata | undefined {
+  for (const t in tokens) {
+    const token = tokens[t]
+    if (
+      token.tokenIdentifier &&
+      token.tokenIdentifier.id.toString().toLowerCase() ==
+        tokenID.id.toString().toLowerCase()
+    ) {
       return token
     }
   }
---
+++ src/views/Transactions/Nomad/columns/amount.vue
@@ -0,0 +1,60 @@
+<template>
+  <div v-if="token" class="flex flex-row items-center">
+    <div class="flex flex-col items-end leading-4">
+      <div>{{ amt }}</div>
+      <div>{{ token.symbol }}</div>
+    </div>
+    <div class="bg-black bg-opacity-50 rounded-lg p-3 ml-2">
+      <img :src="token.icon" class="h-4" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { BigNumber } from 'ethers'
+import {
+  fromBytes32,
+  getNetworkByDomainID,
+  getTokenByTokenID,
+  toDecimals,
+} from '@/utils'
+import { IndexerTx, TokenMetadata } from '@/config/types'
+
+interface ComponentData {
+  amt: string | undefined
+  token: TokenMetadata | undefined
+}
+
+export default defineComponent({
+  props: {
+    tx: {
+      type: Object as () => IndexerTx,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      amt: undefined,
+      token: undefined,
+    } as ComponentData
+  },
+  mounted() {
+    this.getAmount()
+  },
+  methods: {
+    getAmount() {
+      const tokenIdentifier = {
+        domain: getNetworkByDomainID(this.tx.tokenDomain).name,
+        id: fromBytes32(this.tx.tokenId),
+      }
+      this.token = getTokenByTokenID(tokenIdentifier)!
+      this.amt = toDecimals(
+        BigNumber.from(this.tx.amount),
+        this.token.decimals,
+        6
+      )
+    },
+  },
+})
+</script>
