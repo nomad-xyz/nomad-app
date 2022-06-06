b24e07d6f4c169aa458f8d9b295095fab9c868c6
Imti Majeed <imtiazmaj@gmail.com>
2022-06-06 12:56:43 -0700

    chore: re-add copy

--- src/views/Transfer/Transfer.pending.vue
+++ src/views/Transfer/Transfer.pending.vue
@@ -5,6 +5,9 @@
       <n-text class="text-[20px] uppercase text-lg font-medium text-white my-5">
         Sending transaction
       </n-text>
+      <n-text class="opacity-70 mb-3">
+        Please continue to your wallet to<br />approve your bridge transaction
+      </n-text>
     </div>
   </div>
 </template>
